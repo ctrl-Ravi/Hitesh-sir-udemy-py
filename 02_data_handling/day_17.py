6663b3fec61976df4221bd550fe6eabdb1786acb
ctrl-Ravi <rp727906@gmail.com>
2025-12-15 22:18:04 +0530

    Challenge: JSON-to-Excel Converter Tool

---
+++ 02_data_handling/day_17.py
@@ -0,0 +1,59 @@
+"""
+ Challenge: JSON-to-Excel Converter Tool
+
+Create a Python utility that reads structured data (like you'd get from an API) from a `.json` file and converts it to a CSV file that can be opened in Excel.
+
+Your program should:
+1. Read from a file named `api_data.json` in the same folder.
+2. Convert the JSON content (a list of dictionaries) into `converted_data.csv`.
+3. Automatically extract field names as CSV headers.
+4. Handle nested structures by flattening or skipping them.
+
+Bonus:
+- Provide feedback on how many records were converted
+- Allow user to define which fields to extract
+- Handle missing fields gracefully
+"""
+
+import json
+import csv
+import os
+
+INPUT_FILE= "api_data.json"
+
+OUTPUT_FILE= f"{INPUT_FILE}.csv"
+
+
+def load_json_data(filename):
+    if not os.path.exists(filename):
+        print("json file not found")
+        return []
+    
+    with open(filename, "r", encoding="utf-8") as f:
+        try:
+            return json.load(f)
+        except:
+            print("Invalid JSON format")
+
+def convert_to_csv(data, OUTPUT_FILE):
+    if not data:
+        print("Not data to convert")
+        return
+    
+    fieldname = list(data[0].keys())
+    with open(OUTPUT_FILE, "w", newline='', encoding="utf-8") as f:
+        writer= csv.DictWriter(f, fieldnames=fieldname)
+        writer.writeheader()
+        for record in data:
+            writer.writerow(record)
+    
+    print(f"COnverted {len(data)} records to {OUTPUT_FILE}")
+
+
+def main():
+    print("Converting JSON to CSV.....")
+    data = load_json_data(INPUT_FILE)
+    convert_to_csv(data, OUTPUT_FILE)
+
+if __name__ == "__main__":
+    main()
